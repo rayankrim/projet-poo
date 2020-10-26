2ee4fe6ee1194d946e6a77e798f534cbfd6eba80
clarayk <yakan.clara@gmail.com>
2020-10-26 12:00:36 +0100

    files added

---
+++ src/Partie1/Addition.java
@@ -0,0 +1,43 @@
+
+package Partie1;
+
+public class Addition extends OperationBinaire {
+
+	public Addition(ExpressionArithmetique eaLeft, ExpressionArithmetique eaRight) {
+		super(eaLeft, eaRight);
+
+	}
+
+	@Override
+	public double calculer() {
+		if (this.eaLeft instanceof VarSymbolique || this.eaRight instanceof VarSymbolique){
+			
+		}
+		return this.eaLeft.calculer() + this.eaRight.calculer();//(2+1)+(4+5)
+		
+	}
+
+	@Override
+	protected ExpressionArithmetique simplifie(ConstRationnelle gauche, ConstEntiere droite) {
+		return new ConstRationnelle(gauche.getNumerateur() * droite.getEntier() + gauche.getDenominateur() * 1,
+				1 * gauche.getDenominateur()).simplifier();
+	}
+
+	@Override
+	protected ExpressionArithmetique simplifie(ConstRationnelle gauche, ConstRationnelle droite) {
+		return new ConstRationnelle(
+				gauche.getNumerateur() * droite.getDenominateur() + gauche.getDenominateur() * droite.getNumerateur(),
+				droite.getDenominateur() * gauche.getDenominateur()).simplifier();
+	}
+
+	@Override
+	protected ExpressionArithmetique simplifie(ConstEntiere gauche, ConstEntiere droite) {
+		return new ConstEntiere(gauche.getEntier() + droite.getEntier()).simplifier();
+	}
+
+	@Override
+	protected ExpressionArithmetique simplifie(ConstEntiere gauche, ConstRationnelle droite) {
+		return simplifie(droite, gauche).simplifier();
+	}
+
+}
---
+++ src/Partie1/ExpressionArithmetique.java
@@ -0,0 +1,6 @@
+package Partie1;
+
+public interface ExpressionArithmetique {
+	public ExpressionArithmetique simplifier();
+	public double calculer();
+}
