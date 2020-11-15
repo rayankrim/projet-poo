5db2eff6019c1b9414877dbbe65c07a2bcbf27c6
Rayan KRIM <rayankrim11@hotmail.com>
2020-11-15 19:46:51 +0100

    var symbo

---
+++ JavaProjet/src/main/java/projet/Addition.java
@@ -0,0 +1,58 @@
+
+package projet;
+
+public class Addition extends OperationBinaire {
+
+	public Addition(ExpressionArithmetique eaLeft, ExpressionArithmetique eaRight) {
+		super(eaLeft, eaRight);
+
+	}
+
+	@Override
+	public double calculer() throws VarSymboliqueException {
+
+		double approximation = this.eaLeft.calculer() + this.eaRight.calculer();
+
+		approximation = Math.round(approximation * 10000);
+
+		return approximation / 10000;
+
+	}
+
+	@Override
+	protected ExpressionArithmetique simplifie(ConstRationnelle gauche, ConstEntiere droite) {
+		return new ConstRationnelle(gauche.getNumerateur() * droite.getEntier() + gauche.getDenominateur() * 1,
+				1 * gauche.getDenominateur()).simplifier();
+	}
+
+	@Override
+	protected ExpressionArithmetique simplifie(ConstRationnelle gauche, ConstRationnelle droite) {
+		return new ConstRationnelle(
+				gauche.getNumerateur() * droite.getDenominateur() + gauche.getDenominateur() * droite.getNumerateur(),
+				droite.getDenominateur() * gauche.getDenominateur()).simplifier();
+	}
+
+	@Override
+	protected ExpressionArithmetique simplifie(ConstEntiere gauche, ConstEntiere droite) {
+		return new ConstEntiere(gauche.getEntier() + droite.getEntier()).simplifier();
+	}
+
+	// @Override
+	// protected ExpressionArithmetique simplifie(VarSymbolique gauche, ConstEntiere
+	// droite) {
+	// return new (this.gauche.getVariable() + this.droite.getEntier()) ;
+	// }
+
+	@Override
+	protected ExpressionArithmetique simplifie(ConstEntiere gauche, ConstRationnelle droite) {
+
+		return simplifie(droite, gauche).simplifier();
+	}
+
+	@Override
+	protected ExpressionArithmetique simplifie(ConstEntiere gauche, VarSymbolique droite) {
+		// TODO Auto-generated method stub
+		return super.simplifie(gauche, droite);
+	}
+
+}
---
+++ JavaProjet/src/main/java/projet/Exponentielle.java
@@ -0,0 +1,22 @@
+package projet;
+
+public class Exponentielle extends OperationUnaire {
+
+	public Exponentielle(ExpressionArithmetique ea) {
+		super(ea);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public double calculer() throws VarSymboliqueException {
+
+		return Math.exp(this.ea.calculer());
+	}
+
+	@Override
+	public boolean egaliteAr(ExpressionArithmetique expr2) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
